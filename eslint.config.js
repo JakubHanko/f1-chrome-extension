bc1a5ffa67d05623e3d5ab35395d87694eb0afa9
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2024-11-11 22:55:01 +0100

    Add import rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import stylisticTs from "@stylistic/eslint-plugin-ts";
+import importPlugin from "eslint-plugin-import";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
@@ -8,7 +9,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: [ "dist" ] },
   {
-    extends: [ js.configs.recommended, ...tseslint.configs.recommended ],
+    extends: [ js.configs.recommended, ...tseslint.configs.recommended, importPlugin.flatConfigs.recommended ],
     files: [ "**/*.{js,ts,tsx}" ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -17,7 +18,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "@stylistic/ts": stylisticTs,
+      "@stylistic/ts": stylisticTs
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -58,6 +59,14 @@ export default tseslint.config(
       "arrow-parens": [ "error", "always" ],
       "keyword-spacing": [ "error", { "before": true, "after": true } ],
       "comma-spacing": [ "error", { "before": false, "after": true } ],
+    },
+    settings: {
+      "import/resolver": {
+        // You will also need to install and configure the TypeScript resolver
+        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+        "typescript": true,
+        "node": true,
+      },
     },
   },
 );
