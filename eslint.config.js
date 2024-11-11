cbf5633ae30b1c70add2fabca9640e24632042b4
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2024-11-11 20:07:53 +0100

    Add typecheck to precommit and format .js

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import stylisticTs from '@stylistic/eslint-plugin-ts'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      '@stylistic/ts': stylisticTs,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@stylistic/ts": stylisticTs,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
       "eqeqeq": ["error", "always"],
@@ -40,6 +40,7 @@ export default tseslint.config(
       }],
       "brace-style": "off",
       "curly": ["error", "all"],
+      "@typescript-eslint/no-unused-vars": "off" // checked by TSC
     },
   },
-)
+);
