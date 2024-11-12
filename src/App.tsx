ed9085f62d0986b979697a3e80e48933081675d2
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2024-11-12 16:11:17 +0100

    Integrate with F1 API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/Arrow";
 import { ArrowDirection } from "./components/ArrowDirection";
 
+const SESSIONS_STORAGE_KEY = "sessions";
+interface Location {
+  lat: number;
+  long: number;
+  country: string;
+}
+
+interface Session {
+  date: string;
+  time: string;
+}
+
+interface Circuit {
+  circuitId: string;
+  url: string;
+  circuitName: string;
+  Location: Location;
+}
+
+interface GrandPrix {
+  Circuit: Circuit;
+  FirstPractice: Session;
+  Qualifying: Session;
+  SecondPractice?: Session;
+  ThirdPractice?: Session;
+  Sprint?: Session;
+  SprintQualifying?: Session;
+  SprintShootout?: Session;
+  date: string;
+  raceName: string;
+  round: string;
+  season: string;
+  time: string;
+  url: string;
+}
+
 const App: React.FC = () => {
   const [ count, setCount ] = useState(0);
+  const [ grandPrix, setGrandPrix ] = useState<GrandPrix[]>([]);
 
-  const decrementCount = () => setCount((count) => count - 1);
-  const incrementCount = () => setCount((count) => count + 1);
+  const decrementCount = () => setCount((count) => count > 0 ? count - 1 : count);
+  const incrementCount = () => setCount((count) => count === grandPrix.length - 1 ? count : count + 1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.jolpi.ca/ergast/f1/2024/races");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const result = await response.json();
+      localStorage.setItem(SESSIONS_STORAGE_KEY, JSON.stringify(result.MRData.RaceTable.Races));
+
+      setGrandPrix(result);
+    };
+
+    const cachedData = localStorage.getItem(SESSIONS_STORAGE_KEY);
+    if (cachedData) {
+      setGrandPrix(JSON.parse(cachedData));
+
+      return;
+    }
+    fetchData(); // Call the async function
+  }, [ ]);
+
+  console.log(grandPrix);
 
   return (
     <>
@@ -15,7 +75,7 @@ const App: React.FC = () => {
         <Button direction={ArrowDirection.LEFT} clicked={decrementCount}/>
         <Button direction={ArrowDirection.RIGHT} clicked={incrementCount}/>
         <br/>
-        count is {count}
+        current GP is: {grandPrix?.[count]?.raceName}
       </div>
     </>
   );
