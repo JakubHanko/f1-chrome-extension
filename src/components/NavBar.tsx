026df0e37c150d873e947a39a31a68214b217b20
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2024-11-14 16:58:59 +0100

    Fix button hover in extension context

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { AppShell, Button, Flex } from "@mantine/core";
-import "./NavBar.module.css";
+import styles from "./NavBar.module.css";
 
 export function NavBar() {
   return (
@@ -12,10 +12,10 @@ export function NavBar() {
         direction="row"
         wrap="wrap"
       >
-        <Button bg="red" size="xl" radius="xs">
+        <Button className={styles.button} bg="red" size="xl" radius="xs">
                 Calendar
         </Button>
-        <Button bg="red" size="xl" radius="xs">
+        <Button className={styles.button} bg="red" size="xl" radius="xs">
                 Lorem Ipsum
         </Button>
       </Flex>
