17d0d2e06bc88c96db8800e7edfc732c53dc5789
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2024-11-17 07:41:52 +0100

    Add constructors standings

--- src/components/Standings.tsx
+++ src/components/Standings.tsx
@@ -1,16 +1,35 @@
+import { SegmentedControl } from "@mantine/core";
+import { useState } from "react";
 import { StandingsTable } from "./StandingsTable";
+
+type Options = "Drivers" | "Constructors";
 
 export const Standings = () => {
   const currentYear = new Date().getFullYear();
+  const [ option, setOption ] = useState("Drivers");
+  const options: Options[] = [ "Drivers", "Constructors" ];
 
+  // TODO: move the API communication to here
   return (
     <>
-      <StandingsTable
-        header={[ "Pos", "Driver", "Car", "Pts" ]}
-        year={currentYear}
-        endpoint="driverstandings"
+      <SegmentedControl
+        data={options}
+        onChange={setOption}
+        color="f1red"
       />
-      {/* Pos, Team, Pts */}
+      {
+        option === "Drivers"
+          ? <StandingsTable
+            header={[ "Pos", "Driver", "Car", "Pts" ]}
+            year={currentYear}
+            endpoint="driverstandings"
+          />
+          : <StandingsTable
+            header={[ "Pos", "Team", "Pts" ]}
+            year={currentYear}
+            endpoint="constructorstandings"
+          />
+      }
     </>
   );
 };
